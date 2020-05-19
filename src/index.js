d9c66abd20315c039e8031926370a184e514c86b
Guilherme Miyae <gui.fido@gmail.com>
2020-05-19 00:07:05 -0300

    add new providers to index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
+import {createMuiTheme, ThemeProvider} from "@material-ui/core/styles";
+import {createBrowserHistory} from "history";
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {I18nextProvider} from "react-i18next";
+import {Router} from "react-router-dom";
+import App from './App.tsx';
+import i18n from "./i18n";
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const theme = createMuiTheme({
+    palette: {
+        type: 'dark',
+        primary: {
+            light: '#718792',
+            main: '#455a64',
+            dark: '#1c313a',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#63a3ff',
+            main: '#1875d1',
+            dark: '#004a9f',
+            contrastText: '#000',
+        },
+    },
+});
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <I18nextProvider i18n={i18n}>
+            <ThemeProvider theme={theme}>
+                <Router history={history}>
+                    <App/>
+                </Router>
+            </ThemeProvider></I18nextProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
