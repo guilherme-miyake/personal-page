ac8769cd49ccf67356e26267acbdf21c3c02cb1a
Guilherme Miyae <gui.fido@gmail.com>
2020-05-19 00:53:06 -0300

    create SectionHeader component

---
+++ src/components/SectionHeader.tsx
@@ -0,0 +1,46 @@
+import React, {Fragment} from "react";
+
+import {Typography} from "@material-ui/core";
+import Container from "@material-ui/core/Container";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import useTheme from "@material-ui/core/styles/useTheme";
+import {SvgIconComponent} from "@material-ui/icons"
+import SvgIcon from "@material-ui/core/SvgIcon";
+
+const useStyles = makeStyles((theme) => ({
+    title: {
+        padding: "0px",
+        justifyContent: "flex-start",
+        flexDirection: "row",
+        alignItems: "flex-end",
+        display: 'flex',
+        margin: "2px 0px"
+    },
+    text:{
+        margin: "0px 0px 10px 0px"
+    },
+    icon:{
+        fontSize: "80px",
+        margin: "0px 10px 0px 0px"
+    },
+    line: {
+        backgroundColor: theme.palette.text.primary,
+        display: "flex",
+        height: "8px",
+        padding: "0",
+        borderRadius: "5px"
+    }
+}));
+
+export default function SectionHeader(props: { title: string, icon: SvgIconComponent}) {
+    const theme = useTheme()
+    const classes = useStyles(theme)
+
+    return <Fragment>
+        <Container className={classes.title}>
+            <SvgIcon className={classes.icon} component={props.icon}/>
+            <Typography className={classes.text}  variant="h4">{props.title}</Typography>
+        </Container>
+        <Container className={classes.line}/>
+    </Fragment>
+}
