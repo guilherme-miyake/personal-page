5cb5148cf5ae04795fda862716e9688ffad6c546
Guilherme Miyae <gui.fido@gmail.com>
2020-05-28 21:05:40 -0300

    better naming for hook parameters

--- src/hooks/useText.tsx
+++ src/hooks/useText.tsx
@@ -1,7 +1,7 @@
 import {useTranslation} from "react-i18next";
 import {Translation} from "../locales";
 
-export default function useText<TranslatableText>(context: keyof Translation) {
-    const {t,} = useTranslation(context)
-    return t as (translatableText: keyof TranslatableText) => string
+export default function useText<PageContext>(pageContext: keyof Translation) {
+    const {t,} = useTranslation(pageContext)
+    return t as (translatableText: keyof PageContext) => string
 }
