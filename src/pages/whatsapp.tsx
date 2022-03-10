1f561882bc4eb330ff5813c9586a6ecca454c325
Guilherme Toshio Miyake <toshio@dinerama.com.br>
2022-03-10 14:57:31 -0300

    fix: base64 conversion

--- src/pages/whatsapp.tsx
+++ src/pages/whatsapp.tsx
@@ -7,7 +7,7 @@ import {Translation} from "../locales";
 
 export default function About(props: { location: any }) {
     const text = useText<Translation["Home"]>("Home")
-    let link = atob("aHR0cHM6Ly9hcGkud2hhdHNhcHAuY29tL3NlbmQvP3Bob25lPTU1MTE5OTQ4NTkzMDImdGV4dD0=")
+    let link = Buffer.from("aHR0cHM6Ly9hcGkud2hhdHNhcHAuY29tL3NlbmQvP3Bob25lPTU1MTE5OTQ4NTkzMDImdGV4dD0=").toString('base64')
     let message = encodeURIComponent(text("greetings"))
     navigate(link + message)
 
