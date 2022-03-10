9539c1853d4838b8cf7e1fb2813243dee9cd1c52
Guilherme Toshio Miyake <toshio@dinerama.com.br>
2022-03-10 15:27:54 -0300

    stop using navigate

--- src/pages/whatsapp.tsx
+++ src/pages/whatsapp.tsx
@@ -1,6 +1,5 @@
 import React, {Fragment, useEffect} from "react";
 import Phone from "../components/contactBadges/Phone";
-import {navigate} from "gatsby";
 import useText from "../hooks/useText";
 import {Translation} from "../locales";
 import {Buffer} from "buffer";
@@ -10,7 +9,7 @@ export default function About(props: { location: any }) {
     const text = useText<Translation["Home"]>("Home")
     let link = Buffer.from("aHR0cHM6Ly9hcGkud2hhdHNhcHAuY29tL3NlbmQvP3Bob25lPTU1MTE5OTQ4NTkzMDImdGV4dD0=", 'base64').toString()
     let message = encodeURIComponent(text("greetings"))
-    useEffect(()=> {navigate(link + message)})
+    useEffect(()=> { window.location.href = link + message})
     return <Fragment>
         <Phone/>
     </Fragment>
