be9f7cd64d116d6849282114c22728da490d6b99
Guilherme Toshio Miyake <toshio@dinerama.com.br>
2022-03-10 17:01:16 -0300

    generate robotos

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,80 @@
-module.exports = {
-  siteMetadata: {
-    title: `Guilherme Toshio Miyake - Personal Page`,
-    description: `A personal page built with Gatsby, React, Typescript and MaterialUI.`,
-    author: `@guilherme-miyake`,
-    icon: './src/images/favicon.ico'
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: 'gatsby-plugin-react-svg',
-      options: {
-        rule: {
-          include: `${__dirname}/src/assets/svg`,
+const {
+    NODE_ENV,
+    URL: NETLIFY_SITE_URL = 'https://www.example.com',
+    DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+    CONTEXT: NETLIFY_ENV = NODE_ENV
+} = process.env;
+const isNetlifyProduction = NETLIFY_ENV === 'production';
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
 
-        }
-      }
+module.exports = {
+    siteMetadata: {
+        title: `Guilherme Toshio Miyake - Personal Page`,
+        description: `A personal page built with Gatsby, React, Typescript and MaterialUI.`,
+        author: `@guilherme-miyake`,
+        siteUrl
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/assets/images`,
-      },
-    },
-    `gatsby-plugin-preact`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    'gatsby-plugin-styled-components',
-    {
-      resolve: `gatsby-plugin-material-ui`,
-      options: {
-        stylesProvider: {
-          injectFirst: false,
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: 'gatsby-plugin-robots-txt',
+            options: {
+                resolveEnv: () => NETLIFY_ENV,
+                env: {
+                    production: {
+                        policy: [{userAgent: '*'}]
+                    },
+                    'branch-deploy': {
+                        policy: [{userAgent: '*', disallow: ['/']}],
+                        sitemap: null,
+                        host: null
+                    },
+                    'deploy-preview': {
+                        policy: [{userAgent: '*', disallow: ['/']}],
+                        sitemap: null,
+                        host: null
+                    }
+                }
+            }
         },
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+        {
+            resolve: 'gatsby-plugin-react-svg',
+            options: {
+                rule: {
+                    include: `${__dirname}/src/assets/svg`,
+
+                }
+            }
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/assets/images`,
+            },
+        },
+        `gatsby-plugin-preact`,
+        `gatsby-transformer-sharp`,
+        {
+            resolve: `gatsby-plugin-sharp`,
+            options: {
+                icon: './src/assets/images/favicon.ico',
+                // Set to false to allow builds to continue on image errors
+                failOnError: true,
+                // deprecated options and their defaults:
+            },
+        },
+        'gatsby-plugin-styled-components',
+        {
+            resolve: `gatsby-plugin-material-ui`,
+            options: {
+                stylesProvider: {
+                    injectFirst: false,
+                },
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
 }
