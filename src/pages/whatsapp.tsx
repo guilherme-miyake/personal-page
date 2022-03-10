450217145125fd8cc5f050f29b5d029ceb8b567a
Guilherme Toshio Miyake <toshio@dinerama.com.br>
2022-03-10 15:18:45 -0300

    fix link

--- src/pages/whatsapp.tsx
+++ src/pages/whatsapp.tsx
@@ -3,11 +3,12 @@ import Phone from "../components/contactBadges/Phone";
 import {navigate} from "gatsby";
 import useText from "../hooks/useText";
 import {Translation} from "../locales";
+import {Buffer} from "buffer";
 
 
 export default function About(props: { location: any }) {
     const text = useText<Translation["Home"]>("Home")
-    let link = Buffer.from("aHR0cHM6Ly9hcGkud2hhdHNhcHAuY29tL3NlbmQvP3Bob25lPTU1MTE5OTQ4NTkzMDImdGV4dD0=").toString('base64')
+    let link = Buffer.from("aHR0cHM6Ly9hcGkud2hhdHNhcHAuY29tL3NlbmQvP3Bob25lPTU1MTE5OTQ4NTkzMDImdGV4dD0=", 'base64').toString()
     let message = encodeURIComponent(text("greetings"))
     useEffect(()=> {navigate(link + message)})
     return <Fragment>
