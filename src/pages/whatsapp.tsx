2bb354f9a3c858fcd0419660b8d725b6d0b7fc9e
Guilherme Toshio Miyake <toshio@dinerama.com.br>
2022-03-10 15:00:04 -0300

    fix redirection call

--- src/pages/whatsapp.tsx
+++ src/pages/whatsapp.tsx
@@ -1,4 +1,4 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useEffect} from "react";
 import Phone from "../components/contactBadges/Phone";
 import {navigate} from "gatsby";
 import useText from "../hooks/useText";
@@ -9,8 +9,7 @@ export default function About(props: { location: any }) {
     const text = useText<Translation["Home"]>("Home")
     let link = Buffer.from("aHR0cHM6Ly9hcGkud2hhdHNhcHAuY29tL3NlbmQvP3Bob25lPTU1MTE5OTQ4NTkzMDImdGV4dD0=").toString('base64')
     let message = encodeURIComponent(text("greetings"))
-    navigate(link + message)
-
+    useEffect(()=> {navigate(link + message)})
     return <Fragment>
         <Phone/>
     </Fragment>
