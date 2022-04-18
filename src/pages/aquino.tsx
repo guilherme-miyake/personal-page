03b1b5b96dbf61751e5eaf526228220d7f501c8a
Guilherme Toshio Miyake <toshio@dinerama.com.br>
2022-04-18 18:27:32 -0300

    feature: add aquino page

--- src/pages/aquino.tsx
+++ src/pages/aquino.tsx
@@ -5,20 +5,19 @@ import {Paper, Typography} from "@material-ui/core";
 
 export default function About(props: { location: any }) {
     var go_to = () => {
-        window.location.href = 'https://nfe.prefeitura.sp.gov.br/contribuinte/notaprint.aspx?inscricao=72535130' + location.search.replace("?", "&")
+        window.location.href = 'https://aquino-birthday.herokuapp.com/static/aquino.html'
     }
     useEffect(go_to)
     return <Fragment>
         <SEO
-            title={"Nota Fiscal"}
-            description={"Abrir a nota fiscal"}
+            title={"Parabens Aquino!"}
+            description={"Ao melhor vizinho que só fui conhecer no role"}
         />
         <div onClick={go_to}>
             <Paper>
                 <div style={{padding: "2em"}}>
                     <Typography>
                         Redirecionando...<br/>
-                        Clique aqui para acessar a nota no site da prefeitura
                     </Typography>
                 </div>
             </Paper>
